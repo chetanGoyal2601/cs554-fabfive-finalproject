61de3e851c2e40f7ca85eccc73ad88278610d53f
Jefferson Dias <diasjefferson2@gmail.com>
2022-12-16 20:40:22 -0500

    Added user login validation in backend and Create

--- Backend/routes/event.js
+++ Backend/routes/event.js
@@ -11,6 +11,13 @@ const upload = multer({ dest: "images/" });
 router.post("/", upload.single("image"), async (req, res) => {
   try {
     //validations
+    // if (!userId) throw { message: "User not logged in", code: 403 };
+  } catch (e) {
+    return res.status(400).json({ error: e.message ? e.message : e });
+  }
+
+  try {
+    //validations
     req.body.title = validations.checkString(req.body.title, "Title");
     req.body.description = validations.checkString(
       req.body.description,
@@ -55,6 +62,18 @@ router.post("/", upload.single("image"), async (req, res) => {
   }
 });
 
+router.get("/user", async (req, res) => {
+  try {
+    let userId = { userId: "false" };
+    // userData.calcAvgRating(23);
+    return res.json(userId);
+  } catch (e) {
+    return res.status(e.code || 404).json({
+      error: e.message ? e.message : e,
+    });
+  }
+});
+
 router.get("/page/:page", async (req, res) => {
   try {
     req.params.page = validations.checkNumber(req.params.page, "Page Number");
@@ -89,6 +108,12 @@ router.get("/:id", async (req, res) => {
 
 router.patch("/:id", async (req, res) => {
   try {
+    //validations
+    // if (!userId) throw { message: "User not logged in", code: 403 };
+  } catch (e) {
+    return res.status(400).json({ error: e.message ? e.message : e });
+  }
+  try {
     req.params.id = validations.checkId(req.params.id, "Event ID");
     // req.body.data.userId = validations.checkId(req.body.data.userId, "User ID");
   } catch (e) {
@@ -118,6 +143,12 @@ router.patch("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   try {
+    //validations
+    // if (!userId) throw { message: "User not logged in", code: 403 };
+  } catch (e) {
+    return res.status(400).json({ error: e.message ? e.message : e });
+  }
+  try {
     req.params.id = validations.checkId(req.params.id, "Event ID");
     // req.body.userId = validations.checkId(req.body.userId, "User ID");
   } catch (e) {
@@ -146,6 +177,12 @@ router.delete("/:id", async (req, res) => {
 });
 
 router.post("/rating/:id", async (req, res) => {
+  try {
+    //validations
+    // if (!userId) throw { message: "User not logged in", code: 403 };
+  } catch (e) {
+    return res.status(400).json({ error: e.message ? e.message : e });
+  }
   try {
     req.params.id = validations.checkId(req.params.id, "Event ID");
     // req.body.data.userId = validations.checkId(req.body.data.userId, "User ID"); // Need to verify body.data or body
--- frontend/src/components/CreateEvent.js
+++ frontend/src/components/CreateEvent.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
 import axios from "axios";
 import Form from "react-bootstrap/Form";
 import dayjs from "dayjs";
@@ -6,18 +7,38 @@ import TextField from "@mui/material/TextField";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import Alert from "@mui/material/Alert";
 
 const CreateEvent = () => {
-  let path = "http://localhost:3001/";
   const [file, setFile] = useState();
-  const [response, setResponse] = useState();
   const [description, setDescription] = useState("");
   const [title, setTitle] = useState("");
+  const [response, setResponse] = useState();
   // const [time, setTime] = useState("");
   const [capacity, setCapacity] = useState("");
   const [address, setAddress] = useState("");
   const [address2, setAddress2] = useState("");
   const [value, setValue] = React.useState(dayjs(new Date()));
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { data } = await axios.get(`http://localhost:3001/event/user`);
+        if (data) {
+          if (data.userId) setLoggedInUser(data.userId);
+
+          setIsError(false);
+        }
+      } catch (e) {
+        setIsError(true);
+        setErrorMessage(e.response.data.error);
+      }
+    }
+    fetchData();
+  });
 
   const handleChange = (newValue) => {
     setValue(newValue);
@@ -34,131 +55,117 @@ const CreateEvent = () => {
     formData.append("address", address);
     formData.append("address2", address2);
 
-    const result = await axios.post("http://localhost:3001/event", formData, {
-      headers: { "Content-Type": "multipart/form-data" },
-    });
-    setResponse(result.data);
+    try {
+      const result = await axios.post("http://localhost:3001/event", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      setResponse(result.data);
+    } catch (e) {
+      setIsError(true);
+      setErrorMessage(e.response.data.error);
+    }
   };
 
-  return (
-    <div className="App">
-      <Form onSubmit={submit}>
-        <input
-          filename={file}
-          onChange={(e) => setFile(e.target.files[0])}
-          type="file"
-          accept="image/*"
-          alt="Jeff"
-          required
-        ></input>
-        <br />
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Title : </Form.Label>
-          <br />
-          <Form.Control
-            onChange={(e) => setTitle(e.target.value)}
-            type="text"
-            placeholder="My Title"
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Description : </Form.Label>
-          <br />
-          <Form.Control
-            onChange={(e) => setDescription(e.target.value)}
-            type="text"
-            as="textarea"
-            rows={3}
-            cols={100}
-            required
-          />
-        </Form.Group>
-        {/* <label>Description</label>
-        <input
-          onChange={(e) => setDescription(e.target.value)}
-          type="text"
-        ></input> */}
-        {/* <label>Title</label>
-        <input onChange={(e) => setTitle(e.target.value)} type="text"></input>
-        <br /> */}
-        {/* <label>Time</label>
-        <input onChange={(e) => setTime(e.target.value)} type="text"></input>
-        <br /> */}
-        <Form.Group className="mb-3" controlId="formGridAddress1">
-          <Form.Label>Address : </Form.Label>
-          <br />
-          <Form.Control
-            onChange={(e) => setAddress(e.target.value)}
-            type="text"
-            placeholder="1234 Main St"
-            required
-          />
-        </Form.Group>
+  if (response && response._id) {
+    return <Alert severity="success">Event created Successfully.!</Alert>;
+  }
 
-        <Form.Group className="mb-3" controlId="formGridAddress2">
-          <Form.Label>Address 2 : </Form.Label>
-          <br />
-          <Form.Control
-            onChange={(e) => setAddress2(e.target.value)}
-            type="text"
-            placeholder="Apartment, studio, or floor"
-          />
-        </Form.Group>
-        {/* <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Time : </Form.Label>
-          <br />
-          <Form.Control
-            onChange={(e) => setTime(e.target.value)}
-            type="text"
-            placeholder="Time"
-          />
-        </Form.Group> */}
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Capacity : </Form.Label>
-          <br />
-          <Form.Control
-            onChange={(e) => setCapacity(e.target.value)}
-            type="number"
-            placeholder="Capacity"
+  if (isError)
+    return (
+      <Alert severity="error">
+        {errorMessage ? errorMessage : "Error! Could not create event."}
+      </Alert>
+    );
+
+  if (loggedInUser && loggedInUser === "false") {
+    console.log("Redirect");
+    //redirect to Login
+    return <Navigate to="/" />;
+  } else {
+    return (
+      <div className="App">
+        <Form onSubmit={submit}>
+          <input
+            filename={file}
+            onChange={(e) => setFile(e.target.files[0])}
+            type="file"
+            accept="image/*"
+            alt="Jeff"
             required
-          />
-        </Form.Group>
-        {/* <label>Capacity</label>
-        <input
-          onChange={(e) => setCapacity(e.target.value)}
-          type="number"
-        ></input>*/}
+          ></input>
+          <br />
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Title : </Form.Label>
+            <br />
+            <Form.Control
+              onChange={(e) => setTitle(e.target.value)}
+              type="text"
+              placeholder="My Title"
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Description : </Form.Label>
+            <br />
+            <Form.Control
+              onChange={(e) => setDescription(e.target.value)}
+              type="text"
+              as="textarea"
+              rows={3}
+              cols={100}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formGridAddress1">
+            <Form.Label>Address : </Form.Label>
+            <br />
+            <Form.Control
+              onChange={(e) => setAddress(e.target.value)}
+              type="text"
+              placeholder="1234 Main St"
+              required
+            />
+          </Form.Group>
 
-        <br />
-        <br />
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateTimePicker
-            label="Event Date & Time"
-            value={value}
-            onChange={handleChange}
-            type="datetime-local"
-            minDate={new Date()}
-            maxDate={new Date().setFullYear(2023)}
-            minTime={dayjs("2018-01-01T08:00")}
-            renderInput={(params) => <TextField {...params} />}
-          />
-        </LocalizationProvider>
-        <br />
-        <button type="submit">Submit</button>
-      </Form>
-
-      {response && response.title && <p>{response.title}</p>}
-      {response && response.description && <p>{response.description}</p>}
-      {response && response.time && <p>{response.time}</p>}
-      {response && response.capacity && <p>{response.capacity}</p>}
-      {response && response.address && <p>{response.address}</p>}
-      {response && response.address2 && <p>{response.address2}</p>}
-      {response && response.image && (
-        <img alt="Jeff" src={path + response.image} />
-      )}
-    </div>
-  );
+          <Form.Group className="mb-3" controlId="formGridAddress2">
+            <Form.Label>Address 2 : </Form.Label>
+            <br />
+            <Form.Control
+              onChange={(e) => setAddress2(e.target.value)}
+              type="text"
+              placeholder="Apartment, studio, or floor"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Capacity : </Form.Label>
+            <br />
+            <Form.Control
+              onChange={(e) => setCapacity(e.target.value)}
+              type="number"
+              placeholder="Capacity"
+              required
+            />
+          </Form.Group>
+          <br />
+          <br />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateTimePicker
+              label="Event Date & Time"
+              value={value}
+              onChange={handleChange}
+              type="datetime-local"
+              minDate={new Date()}
+              maxDate={new Date().setFullYear(2023)}
+              minTime={dayjs("2018-01-01T08:00")}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </LocalizationProvider>
+          <br />
+          <button type="submit">Submit</button>
+        </Form>
+      </div>
+    );
+  }
 };
 
 export default CreateEvent;
