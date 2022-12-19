5c6182a1568f83b4290f705544b6f7000c39e1e4
chetanGoyal2601 <chetangoyal0181@gmail.com>
2022-12-19 17:30:50 -0500

    Added instruction to login/signup

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,19 +1,28 @@
+import { useOutletContext } from "react-router-dom";
 const Home = () => {
-  return(
-
-    <div className='Home-Page-Image'>
-      <div className='row d-flex p-4 '>
-        <div className='col flex-col'>
-          <img alt="MakeEventHappen" src={require('../img/logo_transparent.png')} className="img-size"/>
+  const userDetails = useOutletContext();
+  return (
+    <div className="Home-Page-Image">
+      <div className="row d-flex p-4 ">
+        <div className="col flex-col">
+          {!userDetails && (
+            <h1 style={{ color: "black" }}>
+              <b>Please signup/login to access the events!</b>
+            </h1>
+          )}
+          <img
+            alt="MakeEventHappen"
+            src={require("../img/logo_transparent.png")}
+            className="img-size"
+          />
         </div>
         {/* <div className='col flex-col'>
           <img alt="MakeEventHappen" src={require('../img/Stevens-Logo.png')} className="img-size"/>
           <p className='quote'>" Bright Lights <br/> & Party Nights !!!! "</p>
         </div> */}
       </div>
-     
     </div>
-  )
-}
+  );
+};
 
 export default Home;
