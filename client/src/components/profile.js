de2d8feb59d3641079b078b7b9e6e9ca489310fe
Devshree Parikh <devshree.parikh@gmail.com>
2022-12-20 15:54:55 -0500

    rohit profile page design

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { useOutletContext } from 'react-router-dom';
 import { Routes,Route, Link, useParams,useNavigate } from 'react-router-dom';
 import 
-{ Card,
+{
  CardActionArea, 
  CardContent, 
  CardMedia, 
@@ -13,6 +13,9 @@ import
  Typography, 
  makeStyles} from '@material-ui/core';
 import '../App.css';
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const useStyles = makeStyles({
 	card: {
@@ -59,7 +62,10 @@ export default function Form() {
   let card2= null;
   let card3 = null;
   let card4 = null;
-  async function onSubmit4 () {
+
+
+  useEffect(() => {
+    async function fetchData(){
     let data2={}
     data2.email=auth.email
     console.log(data2)
@@ -70,22 +76,26 @@ export default function Form() {
       setRsvp_past(res?.data?.reserve_past);
       setRsvp_future(res?.data?.reserve_future);
     }
+    fetchData();
+}, []);
+
+
+
+
+
     const buildCard = (show) => {
       return (
-        <Grid item xs={12} sm={6} md={4} lg={3} xl={2}  >
-          <Card onClick={() => navigate(`/event/${show._id}`)} className={classes.card} variant='outlined' component='div' >
+        <Grid className="mb-3" item xs={12} sm={6} md={4} lg={3} xl={2}>
+          <Card onClick={() => navigate(`/event/${show._id}`)} className={classes.card} variant="light" bg="light" text='dark' >
             <CardActionArea component='div'>
-              {/* <Link to={`/comics/${show.id}`}> */}
-               
-  
                               <CardContent>
-                                  <Typography className={classes.titleHead} gutterBottom variant='h6' component='h2'>
+                                  <p style={{fontSize:"20px",fontWeight:"700"}}>
                                       {show.title}
-                                  </Typography>
+                                  </p>
                                   <Typography variant='body2' color='black' component='p' className='map'>
                                       {show.description ? show.description.replace(regex, '').substring(0, 139) + '...' : 'No Description'}
                     <br></br>
-                    <span className='map'><u>More Info</u></span>
+                    <p className="text-uppercase" style={{fontSize:"13px",fontWeight:"600",color:"crimson"}}>More Info..</p>
                                   </Typography>
                               </CardContent>
                           {/* </Link> */}
@@ -95,71 +105,103 @@ export default function Form() {
       );
     };
 
+    if (host_past) {
+      if (host_past.length === 0)
+      {
+      card1 ="no past hosted events"
+      }
     card1 =
     host_past &&
     host_past.map((show) => {
         return buildCard(show);
     });
+  }
+
+  if (host_future) {
+    if (host_future.length === 0)
+    {
+      card2 ="no future hosted events"
+      }
     card2 =
     host_future &&
     host_future.map((show) => {
         return buildCard(show);
     });
+  }
+
+  if (rsvp_past) {
+    if (rsvp_past.length === 0)
+    {
+      card3 ="no past rsvp events"
+      }
     card3 =
     rsvp_past &&
     rsvp_past.map((show) => {
         return buildCard(show);
     });
+  }
+
+  if (rsvp_future) {
+    if (rsvp_future.length === 0)
+    {
+      card4 ="no future rsvp events"
+      }
     card4 =
     rsvp_future &&
     rsvp_future.map((show) => {
         return buildCard(show);
     });
+  }
 
   return (
     <section>
       {data !==null ?
-      (<div>
-        <h1>Name : {data.name}</h1>
-        <h1>Email : {data.email}</h1>
-        <h1>Gender : {data.gender}</h1>
-        <h1>Rating : {data.rating}</h1>
-
-        <Grid container className={classes.grid} spacing={5}>
-          <div>
-          <h1>Hosted events - past</h1>
-                        {card1}
-                        <br></br>
-                        <br></br>
-                        </div>
-                        <div>
-                        <h1>Hosted events - future</h1>
-                        {card2}
-                        <br></br>
-                        <br></br>
-                        </div>
-                        <div>
-                        <h1>RSVP'd events - past</h1>
+      (<div  className="concert-Background">
+        <Card  variant="secondary" text="dark" className='mb-3 '
+              style={{
+                width: "50rem",
+                height: "auto",
+                alignItems: "center",
+                marginLeft: "auto",
+                marginRight: "auto",
+                paddingTop: "auto",
+                paddingBottom: "auto",
+                overflow: "hidden",
+                marginTop:"auto",
+                borderRadius:"20px",
+                opacity:"0.8"
+              }}>
+          <p  className="text-uppercase" style={{fontSize:"28px",fontWeight:"600",fontStyle:"italic"}}>{data.name}</p>
+          <p style={{fontSize:"23px",fontWeight:"500"}}>Email : {data.email}</p>
+          <p style={{fontSize:"23px",fontWeight:"500"}}>Gender : {data.gender}</p>
+          <p style={{fontSize:"23px",fontWeight:"500"}}>Rating : {data.rating}</p>
+        </Card>
+        <Grid item lg={8} container className={classes.grid} style={{marginLeft:"auto",marginRight:"auto",alignItems:"center"}}>
+                        <Row  className='mb-3 justify-content-center w-100'>
+                        <p className="text-uppercase" style={{fontSize:"28px",fontWeight:"bolder",fontStyle:"italic",color:"black",background:"white",opacity:"0.7",borderRadius:"20px"}}>Hosted Events in Past</p>
+                            {card1}
+                        </Row>
+                        <Row className='mb-3 justify-content-center w-100'>
+                         
+                        <p className="text-uppercase" style={{fontSize:"28px",fontWeight:"bolder",fontStyle:"italic",color:"black",background:"white",opacity:"0.7",borderRadius:"20px"}}>Upcoming Future Events</p>
+                          {card2}
+                         
+                        </Row>
+                        <Row className='mb-3 justify-content-center w-100'>
+                        
+                        <p className="text-uppercase" style={{fontSize:"28px",fontWeight:"bolder",fontStyle:"italic",color:"black",background:"white",opacity:"0.7",borderRadius:"20px"}}>RSVP'd Events from Past</p>
                         {card3}
-                        <br></br>
-                        <br></br>
-                        </div>
-                        <div>
-                        <h1>RSVP'd events - future</h1>
+                       
+                        </Row>
+                        <Row className='mb-3 justify-content-center w-100'>
+                        <p className="text-uppercase" style={{fontSize:"28px",fontWeight:"bolder",fontStyle:"italic",color:"black",background:"white",opacity:"0.7",borderRadius:"20px"}}>RSVP'd Events in Future</p>
                         {card4}
-                        <br></br>
-                        <br></br>
-                        </div>
+                        </Row>
                     </Grid>
       </div>):("")}
-      <div>
-      <button  className='project-btn project-btn-secondary' onClick={onSubmit4}>Reveal</button>
-      </div>
+  
     </section>
   )
 
   
 }
-
-
-
